0af530d30953efd3248fc6e2dfc2826d8c6e58dc
Esha-Sherring <esherring2016@gmail.com>
2021-03-03 12:09:58 +0530

    basic implementation of JSON server

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,28 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () =>{
-    const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-    const [name, setName] = useState('mario');
-    const handleDelete = (id) =>{
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
+    
 
     useEffect(() =>{
-        console.log('use effect ran');
-        console.log(blogs);
-    }, [name])
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs').then(res =>{
+           return res.json();
+       }).then(data =>{
+           setBlogs(data);
+           setIsPending(false);
+
+       })
+    }, [])
+        }, 3000);
+       
 
     return (
         <div className="home">
-        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-        <button onClick={() => setName('luigi')}>change name</button>
+        { isPending && <div>Fetching Blogs.....</div>}
+        {blogs && <BlogList blogs={blogs} title="All blogs"  />}
+        
         </div>
     );
 }
